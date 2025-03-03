38cc6ec3d9d1d332a37864380cae44c6a1cdb1b6
Ayush105199 <119776681+Ayush105199@users.noreply.github.com>
2025-03-03 19:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 127.7 MB (77.25%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,33 +2,60 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n=nums.size();
-        vector<int>p;
-        vector<int>q;
-        vector<int>r;
-        for(int i=0;i<n;i++)
+        vector<int>p(n);
+        int c=0;
+        int index=0;
+        int i;
+        for(i=0;i<n;i++)
         {
             if(nums[i]<pivot)
-            p.push_back(nums[i]);
+            {
+                p[index++]=nums[i];
+            }
             else if(nums[i]==pivot)
-            r.push_back(nums[i]);
-            else
-            q.push_back(nums[i]);
+            c++;
         }
-        nums.clear();
-        for(int i=0;i<p.size();i++)
+        while(c--)
         {
-            nums.push_back(p[i]);
+            p[index++] = pivot;
         }
-        for(int i=0;i<r.size();i++)
+        for(i=0;i<n;i++)
         {
-            nums.push_back(r[i]);
-        }
-        for(int i=0;i<q.size();i++)
-        {
-            nums.push_back(q[i]);
+            if(nums[i]>pivot)
+            {
+                p[index++]=nums[i];
+            }
+
         }
 
-        return nums;
+
+        // vector<int>q;
+        // vector<int>r;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]<pivot)
+        //     p.push_back(nums[i]);
+        //     else if(nums[i]==pivot)
+        //     r.push_back(nums[i]);
+        //     else
+        //     q.push_back(nums[i]);
+        // }
+        // nums.clear();
+        // for(int i=0;i<p.size();i++)
+        // {
+        //     nums.push_back(p[i]);
+        // }
+        // for(int i=0;i<r.size();i++)
+        // {
+        //     nums.push_back(r[i]);
+        // }
+        // for(int i=0;i<q.size();i++)
+        // {
+        //     nums.push_back(q[i]);
+        // }
+
+        // return nums;
+        return p;
 
         
         
