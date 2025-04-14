25ea92cc57504445d893176ff1b1a55975be8738
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-04-14 19:26:42 +0530

    Time: 4 ms (90.51%) | Memory: 11 MB (71.62%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -6,10 +6,10 @@ public:
         for(int i=0;i<n-2;i++)
         {
             for(int j=i+1;j<n-1;j++)
-            {
+            {if(abs(arr[i] - arr[j]) <= a)
                 for(int k=j+1;k<n;k++)
                 {
-                    if(abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k])<= b && abs(arr[i] - arr[k]) <= c )
+                    if(abs(arr[j] - arr[k])<= b && abs(arr[i] - arr[k]) <= c )
                     count++;
                 }
             }
