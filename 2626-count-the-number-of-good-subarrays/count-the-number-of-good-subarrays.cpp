4020a139ee895dbfe2662a2f911157f6f9179349
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-04-17 16:14:50 +0530

    Time: 90 ms (34.28%) | Memory: 79.1 MB (54.61%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
-        const int n=nums.size();
-        unordered_map<int, int> freq;
-        freq.reserve(n);
-        long long ans=0, cnt=0;
+        int n=nums.size();
+        unordered_map<int,int>freq;
+        long long pairs=0,res=0;
+        int left=0;
+        for(int right=0;right<n;right++)
+        {
+            pairs+=freq[nums[right]];
+            freq[nums[right]]++;
 
-        for (int r=0, l=0; r<n; r++) {
-            // increases the pair count by freq[nums[r]]
-            const int x=nums[r];
-            cnt+=freq[x]++;
-
-            // Try to move the left pointer to maintain at least k pairs
-            while (cnt>=k) {
-                // valid subarrays among [l, r0] where r0=r...n-1
-                ans+=(n-r);
-                cnt-=--freq[nums[l]];
-                l++;
+            while(pairs>=k)
+            {
+                res+=(n-right);
+                freq[nums[left]]--;
+                pairs-=freq[nums[left]];
+                left++;
             }
         }
-        return ans;
+return res;
+
+        
     }
 };
