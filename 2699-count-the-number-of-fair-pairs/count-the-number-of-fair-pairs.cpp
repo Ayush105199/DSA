e31413f850eeb26ccec97ecc3c2ab75501771473
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-04-21 00:41:49 +0530

    Time: 53 ms (63.86%) | Memory: 60.5 MB (38.50%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
- long long countFairPairs(vector<int>& nums, int lower, int upper) {
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        
 
-     sort(nums.begin(), nums.end()); // \U0001f5c2️ Sort the array
-     long long totalPairs = countPairsWithSumLessThan(nums, upper + 1) 
-                          - countPairsWithSumLessThan(nums, lower);
-     return totalPairs;
- }
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        long long result = 0;
 
- long long countPairsWithSumLessThan(vector<int> sortedNums, int targetSum) {
+        for (int i = 0; i < n; i++) {
+            int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
+            int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
+            result += (right - left);
+        }
 
-     int leftIndex = 0, rightIndex = sortedNums.size() - 1;
-     long long pairCount = 0;
-
-     while (leftIndex < rightIndex) {
-         int currentSum = sortedNums[leftIndex] + sortedNums[rightIndex];
-
-         if (currentSum < targetSum) {
-             pairCount += (rightIndex - leftIndex); // ✅ All pairs with leftIndex are valid
-             leftIndex++;
-         } else {
-             rightIndex--;
-         }
-     }
-
-     return pairCount;
- }
+        return result;
+    }
 };
+
+ 
