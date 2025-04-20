1bfba103ae98b21960e05c17902dc358ddfc7f1a
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-04-20 18:09:37 +0530

    Time: 4 ms (52.04%) | Memory: 9.8 MB (79.93%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,28 +1,61 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-        string result = "1";
-        for (int i = 1; i < n; ++i) {
-            result = describe(result);
+string desc(string &s)
+{
+    string result="";
+    int count=1;
+    for(int i=1;i<s.length();i++)
+    {
+        if(s[i]==s[i-1])
+        count++;
+        else{
+            result+=to_string(count)+s[i-1];
+            count=1;
         }
-        return result;
     }
+    result+=to_string(count)+s.back();
+    return result;
 
-private:
-    string describe(const string& s) {
-        string res = "";
-        int count = 1;
+}
 
-        for (int i = 1; i < s.length(); ++i) {
-            if (s[i] == s[i - 1]) {
-                count++;
-            } else {
-                res += to_string(count) + s[i - 1];
-                count = 1;
+
+    string countAndSay(int n) {
+        
+        string res="1";
+        
+            for(int i=1;i<n;i++)
+            {
+                res=desc(res);
             }
-        }
-
-        res += to_string(count) + s.back();
-        return res;
+            return res;
+        
+       
     }
 };
+
+// public:
+//     string countAndSay(int n) {
+//         string result = "1";
+//         for (int i = 1; i < n; ++i) {
+//             result = describe(result);
+//         }
+//         return result;
+//     }
+
+// private:
+//     string describe(const string& s) {
+//         string res = "";
+//         int count = 1;
+
+//         for (int i = 1; i < s.length(); ++i) {
+//             if (s[i] == s[i - 1]) {
+//                 count++;
+//             } else {
+//                 res += to_string(count) + s[i - 1];
+//                 count = 1;
+//             }
+//         }
+
+//         res += to_string(count) + s.back();
+//         return res;
+//     }
