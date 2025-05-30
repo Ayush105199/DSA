76df8844cc43012f03c73a75674b9f66872326e5
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-05-31 02:25:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (18.74%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -3,16 +3,27 @@ public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         int n=nums.size();
         int cnt=0;
-        for(int i=0;i<n-2;i++)
+        // for(int i=0;i<n-2;i++)
+        // {
+        //     for(int j=i+1;j<n-1;j++)
+        //     {
+        //         for(int k=j+1;k<n;k++)
+        //         {
+        //             if(nums[j]-nums[i]==diff && nums[k]-nums[j]==diff)
+        //             cnt++;
+        //         }
+        //     }
+        // }
+        // return cnt;
+        unordered_map<int,bool>m;
+        for(auto i :nums)
         {
-            for(int j=i+1;j<n-1;j++)
-            {
-                for(int k=j+1;k<n;k++)
-                {
-                    if(nums[j]-nums[i]==diff && nums[k]-nums[j]==diff)
-                    cnt++;
-                }
-            }
+            m[i]=true;
+        }
+        for(auto i :nums)
+        {
+            if(m[i+diff]==true && m[i-diff]==true)
+            cnt++;
         }
         return cnt;
         
