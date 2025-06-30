90b19879f1debf85f40876aad532287d1e762153
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-06-30 14:01:46 +0530

    Time: 51 ms (13.04%) | Memory: 45.6 MB (13.30%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -1,31 +1,48 @@
+// class Solution {
+// public:
+//     int findLHS(vector<int>& nums) {
+//         int n=nums.size();
+//         map<int,int>m;
+//         for(auto i:nums)
+//         {
+//             m[i]++;
+//         }
+//         int maxi=0;
+//         int q=0;
+//         vector<pair<int,int>>p(m.begin(),m.end());
+//         for(int i=0;i<p.size()-1;i++)
+//         {
+//             if((p[i+1].first - p[i].first)==1){
+//             q=p[i+1].second+p[i].second;
+//             }
+//             maxi=max(maxi,q);
+//         }
+//        return maxi; 
+//     }
+// };
+
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        int n=nums.size();
+        // int n=nums.size();
         map<int,int>m;
-        for(auto i:nums)
+        for(auto i : nums)
         {
             m[i]++;
+
         }
-        // vector<pair<int,int>>p(m.begin(),m.end());
-        // sort(p.begin(),p.end(),[](const pair<int,int>&a,const pair<int,int>&b)
-        // {
-        //     return a.second>b.second;
-        // })
-        // for(auto i:p)
-        // {
-        //     if
-        // }
         int maxi=0;
-        int q=0;
-        vector<pair<int,int>>p(m.begin(),m.end());
-        for(int i=0;i<p.size()-1;i++)
+        for(auto [num,val]:m)
         {
-            if((p[i+1].first - p[i].first)==1){
-            q=p[i+1].second+p[i].second;
+            int q=0;
+            if(m.find(num+1)!=m.end())
+            {
+                q=m[num+1]+m[num];
             }
-            maxi=max(maxi,q);
+            maxi=max(q,maxi);
+
         }
-       return maxi; 
+        return maxi;
+        
     }
 };
