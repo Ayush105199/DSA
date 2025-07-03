e16f3a1cb1c0e84a46dd83106a44db36456103db
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-07-03 23:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (99.62%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,16 +1,29 @@
+// class Solution {
+// public:
+//     char kthCharacter(int k) {
+//         string p ="a";
+//         while(p.size()<=k){
+//         string q="";
+//        for(char ch:p )
+//         {
+//             // q[i]=p[i]+1;
+//             q+=ch+1;
+//         }
+//         p+=q;
+//         }
+//         return p[k-1]; 
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     char kthCharacter(int k) {
-        string p ="a";
-        while(p.size()<=k){
-        string q="";
-       for(char ch:p )
-        {
-            // q[i]=p[i]+1;
-            q+=ch+1;
-        }
-        p+=q;
-        }
-        return p[k-1]; 
+        int s=__builtin_popcount(k-1);
+        return 'a'+s;
+        
+        
     }
 };
