c9e92dc2fba0652aea17d4a619ab69e8d7e9c888
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-07-09 12:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (96.96%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,27 +1,43 @@
+// class Solution {
+// public:
+// int solve(int i,vector<int>&cost,int n,vector<int>&dp)
+// {
+//     if(i>=n)
+//     return 0;
+
+//     if(dp[i]!=-1)
+//     return dp[i];
+
+//     int onestep=cost[i] +solve(i+1,cost,n,dp);
+//     int twostep=cost[i]+solve(i+2,cost,n,dp);
+
+//     return dp[i]=min(onestep,twostep);
+//     // return min(onestep,twostep);
+// }
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         int n=cost.size();
+//         vector<int>dp(n,-1);
+//         int zeroindex=solve(0,cost,n,dp);
+//         int oneindex=solve(1,cost,n,dp);
+//         return min(zeroindex,oneindex);
+//     }
+// };
+
+
 class Solution {
 public:
-// int n=cost.size();
-// int dp[3]={-1};
-// vector<int>dp(n,-1);
-int solve(int i,vector<int>&cost,int n,vector<int>&dp)
-{
-    if(i>=n)
-    return 0;
-
-    if(dp[i]!=-1)
-    return dp[i];
-
-    int onestep=cost[i] +solve(i+1,cost,n,dp);
-    int twostep=cost[i]+solve(i+2,cost,n,dp);
-
-    return dp[i]=min(onestep,twostep);
-    // return min(onestep,twostep);
-}
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int>dp(n,-1);
-        int zeroindex=solve(0,cost,n,dp);
-        int oneindex=solve(1,cost,n,dp);
-        return min(zeroindex,oneindex);
+        int a=cost[0];
+        int b=cost[1];
+        int c=0;
+        for(int i=2;i<n;i++)
+        {
+            c=min(a+cost[i],b+cost[i]);
+            a=b;
+            b=c;
+
+        }
+        return min(a,b);
     }
 };
