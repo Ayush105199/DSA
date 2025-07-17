429eb91bf60905cdef78bdb628e9bd3e5d61cffb
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-07-18 02:26:58 +0530

    Time: 126 ms (32.77%) | Memory: 96.6 MB (51.16%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums, int k) {
+        // int n = nums.size(), res = 0;
+        // vector<vector<int>> dp(k,vector<int> (k,0));
 
+        // for(auto i:nums)
+        // {
+        //     i %= k;
+        //     for(int j = 0 ;j < k;j++)
+        //     {
+        //         dp[j][i] = 1 + dp[i][j];
+        //         res = max(res, dp[j][i]);
+        //     }
+        // }
+        // return res;
 
-
-    
-        int n = nums.size(), res = 0;
-        vector<vector<int>> dp(k,vector<int> (k,0));
-
-        for(auto i:nums)
+        int n=nums.size();
+        vector<vector<int>>dp(k,vector<int>(n,1));
+        int maxsum=1;
+        for(int i=0;i<n;i++)
         {
-            i %= k;
-            for(int j = 0 ;j < k;j++)
+            for(int j=0;j<i;j++)
             {
-                dp[j][i] = 1 + dp[i][j];
-                res = max(res, dp[j][i]);
+                int mod=(nums[i]+nums[j])%k;
+                dp[mod][i]=max(dp[mod][i],1+dp[mod][j]);
+                maxsum=max(maxsum,dp[mod][i]);
             }
         }
-        return res;
+return maxsum;
   
-        
     }
 };
