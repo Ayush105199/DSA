ffcdbb9ea5fe697f6e8018e94bd320ee009f0f51
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-08-02 22:43:20 +0530

    Time: 145 ms (48.54%) | Memory: 157.4 MB (28.45%) - LeetSync

--- 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.cpp
+++ 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.cpp
@@ -1,94 +1,77 @@
-// class Solution {
-// public:
-//     int minimumOperations(vector<int>& nums) {
-//         int n=nums.size();
-//         unordered_map<int,int>m;
-//         for(auto i:nums)
-//         {
-//             m[i]++;
-//         }
-//         vector<pair<int,int>>p(m.begin(),m.end());
-//         sort(p.begin(),p.end(),[](pair<int,int>&a,pair<int,int>&b){
-//             if (a.second != b.second)
-//             return a.second > b.second;
-//         return a.first > b.first; 
-//         });
-
-//         int a=p[0].first;
-//         int b=p[1].first;
-
-//         vector<int>a1;
-//         vector<int>a2;
-//         for(int i=0;i<n;i++)
-//         {
-//             if(i%2!=0)
-//             a1[i]=a;
-//             else
-//             a1[i]=b;
-//         }
-//         for(int i=0;i<n;i++)
-//         {
-//             if(i%2!=0)
-//             a2[i]=b;
-//             else
-//             a2[i]=a;
-//         }
-// int cnt1=0,cnt2=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             if(nums[i]!=a1[i])
-//             cnt1++;
-//             if(nums[i]!=a2[i])
-//             cnt2++;
-
-//         }
-       
-//      return min(cnt1,cnt2);
-//     }
-// };
-
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return 0;
-
-        unordered_map<int, int> evenFreq, oddFreq;
-
-        // Count frequencies at even and odd indices separately
-        for (int i = 0; i < n; ++i) {
-            if (i % 2 == 0)
-                evenFreq[nums[i]]++;
+        
+    
+        int n=nums.size();
+        if(n==1)
+        return 0;
+        unordered_map<int,int>m1,m2;
+        priority_queue<pair<int,int>>p1,p2;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0)
+            m1[nums[i]]++;
             else
-                oddFreq[nums[i]]++;
+            m2[nums[i]]++;
         }
+        int odd=0;
+        int even=0;
+        if(n%2!=0)
+        {
+            even=n/2+1;
+            odd=n/2;
+        }
+        else{
+            even=n/2;
+            odd=n/2;
+        }
+        for(auto i:m1)
+        {
+            p1.push({i.second,i.first});
+        }
+        for(auto i:m2)
+        {
+            p2.push({i.second,i.first});
+        }
+        auto t1=p1.top();
+        p1.pop();
+        auto t2=p2.top();
+        p2.pop();
+        int ans=0;
 
-        // Convert to sorted vectors
-        vector<pair<int, int>> evenVec(evenFreq.begin(), evenFreq.end());
-        vector<pair<int, int>> oddVec(oddFreq.begin(), oddFreq.end());
+        if(t1.second==t2.second)
+        {
+            int op1=even-t1.first;
+            if(!p2.empty())
+            {
+                op1+=odd-p2.top().first;
+            }
+            else
+            {
+                op1+=odd;
+            }
+            int op2=odd-t2.first;
+            if(!p1.empty())
+            {
+                op2+=even-p1.top().first;
+            }
+            else
+            op2+=even;
 
-        // Sort by frequency descending
-        auto cmp = [](pair<int, int>& a, pair<int, int>& b) {
-            return a.second > b.second;
-        };
+            ans=min(op1,op2);
 
-        sort(evenVec.begin(), evenVec.end(), cmp);
-        sort(oddVec.begin(), oddVec.end(), cmp);
-
-        // Get top two elements (to avoid conflicts)
-        int even1 = evenVec.size() > 0 ? evenVec[0].first : -1;
-        int even1Freq = evenVec.size() > 0 ? evenVec[0].second : 0;
-        int even2Freq = evenVec.size() > 1 ? evenVec[1].second : 0;
-
-        int odd1 = oddVec.size() > 0 ? oddVec[0].first : -1;
-        int odd1Freq = oddVec.size() > 0 ? oddVec[0].second : 0;
-        int odd2Freq = oddVec.size() > 1 ? oddVec[1].second : 0;
-
-        // If top elements at even and odd positions are different
-        if (even1 != odd1)
-            return n - (even1Freq + odd1Freq);
+        }
         else
-            // Try second best for either even or odd
-            return n - max(even1Freq + odd2Freq, even2Freq + odd1Freq);
+        {
+            ans=(even-t1.first)+(odd-t2.first);
+
+        }
+        return ans;
+
+
+
+
+
     }
 };
