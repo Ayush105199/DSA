327600eab7ffc56c3785edfa44107c2b01080970
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-08-14 03:02:32 +0530

    Time: 2 ms (66.51%) | Memory: 8.7 MB (97.28%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
 int m=1e9+7;
     bool isPowerOfThree(int n) {
-        int i = 0;
-    while (i<=19) {
-        int p = pow(3, i);
-        if (p == n)
-            return true;
-        if (p > n)
-            break;
-        i++;
-    }
-    return false;
+    //     int i = 0;
+    // while (i<=19) {
+    //     int p = pow(3, i);
+    //     if (p == n)
+    //         return true;
+    //     if (p > n)
+    //         break;
+    //     i++;
+    // }
+    // return false;
+    int maxi=1162261467;
+    return n>0 && maxi%n==0;
     }
 };
