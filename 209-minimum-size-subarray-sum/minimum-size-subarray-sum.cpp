95f0736d80dc9dba44c1ab2e90739c05dc36dc84
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-08-28 00:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (6.63%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -17,32 +17,79 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int prev=nums[0],n=nums.size();
-        vector<int> prefix(n,0);
-        prefix[0]=prev;
-        for(int i=1;i<n;i++){
-            prefix[i]=prev+nums[i];
-            prev=prefix[i];
-        }
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(prefix[i]==target){
-                ans=min(ans,i+1);
-            } else if(prefix[i]>target){
-                int temp=prefix[i];
-                int length=i+1;
-                for(int j=0;j<n;j++){
-                    if(temp-prefix[j]>=target){
-                        length--;
-                    } else{
-                        break;
-                    }
-                }
-                ans=min(ans,length);
+        int n=nums.size();
+        int l=0,j=0;
+        int mini=INT_MAX;
+
+        int sum=0;
+        while(j<n){
+            sum+=nums[j];
+            
+            while(sum>=target)
+            {
+                mini=min(mini,j-l+1);
+                sum-=nums[l];
+                l++;
             }
+            j++;
         }
-        if(ans==INT_MAX) return 0;
-        else return ans;
+        if(mini==INT_MAX)
+        return 0;
+        return mini;
+
+
+
+        // method 1
+        // int prev=nums[0],n=nums.size();
+        // vector<int> prefix(n,0);
+        // prefix[0]=prev;
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=prev+nums[i];
+        //     prev=prefix[i];
+        // }
+        // int ans=INT_MAX;
+        // for(int i=0;i<n;i++){
+        //     if(prefix[i]==target){
+        //         ans=min(ans,i+1);
+        //     } else if(prefix[i]>target){
+        //         int temp=prefix[i];
+        //         int length=i+1;
+        //         for(int j=0;j<n;j++){
+        //             if(temp-prefix[j]>=target){
+        //                 length--;
+        //             } else{
+        //                 break;
+        //             }
+        //         }
+        //         ans=min(ans,length);
+        //     }
+        // }
+        // if(ans==INT_MAX) return 0;
+        // else return ans;
+//    method 2
+        // int n=nums.size();
+    //     int l=0;
+    //     long long sum=0;
+    //     int mini=INT_MAX;
+    //     int j=0;
+    //     while(j<n){
+    //         sum+=nums[j];
+    //         // while(sum>target)
+    //         // {
+    //         //     sum-=nums[l];
+    //         //     l++;
+    //         // }
+    //         while(sum>=target)
+    //         {
+    //             mini=min(mini,j-l+1);
+    //         }
+    //         j++;
+            
+
+    //     }
+    //     if(mini==INT_MAX)
+    //     return 0;
+    // return mini;
     }
 };
         
