37be1f8ef8f93c8e5aa81c24a49f472df11cdb41
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-09-11 23:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.64%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -25,27 +25,42 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
       
-        l = 0
-        curr = head
-        while curr:
-            curr = curr.next
-            l += 1
+        # l = 0
+        # curr = head
+        # while curr:
+        #     curr = curr.next
+        #     l += 1
 
        
-        if n == l:
-            return head.next
+        # if n == l:
+        #     return head.next
 
        
-        curr = head
-        p = l - n
-        for _ in range(p - 1):
-            curr = curr.next
+        # curr = head
+        # p = l - n
+        # for _ in range(p - 1):
+        #     curr = curr.next
 
         
-        if curr.next:
-            curr.next = curr.next.next
+        # if curr.next:
+        #     curr.next = curr.next.next
 
-        return head
+        # return head
 
-           
+        curr=head
+        curr1=head
+        i=0
+        while(i<n):
+            curr=curr.next
+            i+=1
+        if curr==None:
+            return head.next    
+        while curr.next!=None:
+            curr=curr.next
+            curr1=curr1.next
+
+        if(curr1.next):
+            curr1.next=curr1.next.next   
+
+        return head   
 
