25aa201d74317d7be81551dbf27d4dbf990e2198
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-10-08 22:08:39 +0530

    Time: 37 ms (88.04%) | Memory: 94 MB (66.83%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end(),[](const vector<int>&a,const vector<int>&b)
+        {
+            return a[1]<b[1];
+        });
+        int prev=0;
+        int count=1;
+        for(int i=1;i<n;i++)
+        {
+            if(intervals[i][0]>=intervals[prev][1])
+            {
+                count++;
+                prev=i;
+
+            }
+        }
+        return n-count;
+    }
+};
