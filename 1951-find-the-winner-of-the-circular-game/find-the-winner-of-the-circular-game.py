dea4625771ed387c245a8c246cb418d34d9046bc
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2025-12-14 02:22:56 +0530

    Time: 277 ms (5.04%) | Memory: 29.1 MB (8.21%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,7 +1,32 @@
+import numpy as np
 class Solution:
-    def winner(self,n,k):
-        if(n==1):
-            return 0
-        return (self.winner(n-1,k)+k)%n    
+#recursive method
+    # def winner(self,n,k):
+    #     if(n==1):
+    #         return 0
+    #     return (self.winner(n-1,k)+k)%n    
+    # def findTheWinner(self, n: int, k: int) -> int:
+    #     return self.winner(n,k)+1
+    def winner(self,arr,n,index,person_left,k):
+        if person_left==1:
+            for i in range (0,n):
+                if(arr[i]==0):
+                    return i
+
+        kill=(k-1)%person_left
+        while kill > 0:
+            index=(index+1)%n
+            while(arr[index]==1):
+                index=(index+1)%n
+            kill-=1    
+        arr[index]=1
+        while(arr[index]==1):
+            index=(index+1)%n
+        return self.winner(arr,n,index,person_left-1,k)        
+
+
+
     def findTheWinner(self, n: int, k: int) -> int:
-        return self.winner(n,k)+1
+        arr = np.zeros(n, dtype=int)
+        return self.winner(arr,n,0,n,k)+1
+        
