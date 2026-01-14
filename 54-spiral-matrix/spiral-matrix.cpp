124bba740c1100268133926f434dacfb321db445
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2026-01-15 01:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.29%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,54 +1,48 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int left =0;
-        int right=n-1;
-        int top=0;
-        int bottom=m-1;
-        vector<int>result;
+        
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int p=0;
+        int q=0;
+        int r=n-1;
+        int s=m-1;
         int dir=0;
-        while(top<=bottom && left<=right)
+        vector<int>ans;
+        while(p<=r && q<=s)
         {
             if(dir==0)
             {
-                for(int i=left;i<=right;i++)
-                {
-                    result.push_back(matrix[top][i]);
-                }
-                top++;
+                for(int i=q;i<=s;i++)
+                ans.push_back(matrix[p][i]);
+                p++;
             }
             else if(dir==1)
             {
-                for(int i=top;i<=bottom;i++)
-                {
-                    result.push_back(matrix[i][right]);
-                }
-                right--;
+                for(int j=p;j<=r;j++)
+                ans.push_back(matrix[j][s]);
+                s--;
             }
             else if(dir==2)
             {
-                for(int i=right;i>=left;i--)
+                for(int i=s;i>=q;i--)
                 {
-                    result.push_back(matrix[bottom][i]);
+                    ans.push_back(matrix[r][i]);
                 }
-                bottom--;
+                r--;
             }
-            else
-            {
-                for(int i=bottom;i>=top;i--)
+            else{
+                for(int j=r;j>=p;j--)
                 {
-                    result.push_back(matrix[i][left]);
+                    ans.push_back(matrix[j][q]);
                 }
-                left++;
+                q++;
             }
             dir++;
             if(dir==4)
             dir=0;
         }
-        return result;
-        
-        
+        return ans;
     }
 };
