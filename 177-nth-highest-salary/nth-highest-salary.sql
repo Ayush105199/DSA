cfb28d8ff20d562f892567dfec7e9fda9375edd1
Ayush Kesarwani <119776681+Ayush105199@users.noreply.github.com>
2026-02-19 02:24:54 +0530

    Time: 416 ms (69.75%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,12 +1,12 @@
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+
 BEGIN
+DECLARE offset_val INT;
+SET offset_val = N - 1;
   RETURN (
       # Write your MySQL query statement below.
-     with cte as 
-     (select *, dense_rank() over(order by salary desc) as rnk from Employee)
-     select distinct IFNULL(salary,null) from cte where rnk=n
-
-
+      select distinct salary from Employee order by salary desc limit offset_val,1
 
   );
-END
+END 
